7dc6e29585a01dd3de71d12e36b97e97f37601b4
minikelvin <Minikelvin16@hotmail.com>
2018-04-08 16:58:28 +0800

    2018/4/8 add

---
+++ js/common.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('.home_select_tab li').on('touchstart',function () {
+        $(this).addClass('active').siblings('li').removeClass('active');
+        //$("#catatour").val($(this).val());
+    });
+
+
+//    点击展开搜索框
+
+    $('#dropDown_list').focus(function () {
+        $('.dropDown_search').show();
+        // document.activeElement.blur();
+    });
+    $('#dropDown_list').blur(function () {
+        $('.dropDown_search').hide();
+    });
+    //点击搜索框里面的内容。填充到搜索框里面
+    $('.dropDown_li').on('touchstart',function () {
+        $('#dropDown_list').val($(this).text())
+    })
+})
