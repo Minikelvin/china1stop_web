4f036b17f7807f2d0c030495f98ea050a1395cdb
minikelvin <Minikelvin16@hotmail.com>
2018-05-18 14:33:21 +0800

    2018/5/18

--- js/common.js
+++ js/common.js
@@ -7,22 +7,22 @@ $(function () {
 
 //    点击展开搜索框
 
-    $('#dropDown_list,#dropDown_list_screen').focus(function () {
+    $('#dropDown_list,#dropDown_list_screen').bind('focus',function () {
         $('.dropDown_search').show();
-        // document.activeElement.blur();
-    });
-    $('#dropDown_list,#dropDown_list_screen').blur(function () {
+    }).bind('blur',function () {
         $('.dropDown_search').hide();
     });
     //点击搜索框里面的内容。填充到搜索框里面
     $('.dropDown_li').on('touchstart',function () {
         $('#dropDown_list').val($(this).text())
-    })
+    });
 
     $(".blog_ul li").on('touchstart',function() {
         $(this).addClass('active').siblings().removeClass('active');
+        var h_height=$('.blog_left').height();
         var index=this.title;
         var id='#'+'index_'+index;
-        $("html,body").animate({scrollTop: $(id).offset().top}, 500);
+        $("html,body").animate({scrollTop: $(id).offset().top-h_height}, 500);
     });
+
 });
